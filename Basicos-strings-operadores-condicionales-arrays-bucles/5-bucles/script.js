bcd49ead27d02a073a0c7ba802ca355d2ea537cc
Vadithalion <al375550@uji.es>
2020-09-15 12:23:40 +0200

    do while

---
+++ Basicos-strings-operadores-condicionales-arrays-bucles/5-bucles/script.js
@@ -0,0 +1,16 @@
+let pass = 'hola';
+
+console.log(pass);
+/*
+while(pass != 'hola');{
+    pass = prompt('Introduce tu contraseña paspan')
+}
+*/
+
+do{
+    pass = prompt('Introduce tu contraseña paspan')
+}while(pass != 'hola')
+
+
+
+console.log('Fin bucle');
